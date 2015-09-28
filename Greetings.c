e30f7636a47493941398f5954e5fbf1c55aacb3e
xxBeth²xx <ukulelelele@hotmail.fr>
2015-09-28 18:08:18 +0200

    Greetings Anyone !

--- Greetings.c
+++ Greetings.c
@@ -1,5 +1,10 @@
 #include "stdio.h"
 
-void main() {
-  printf("Greetings, World");
+void main(int argc, char** argv) {
+  if (argc == 1) {
+    printf("Greetings, World");
+  }
+  else {
+    printf("Greetings, %s", argv[1]);
+  }
 }
